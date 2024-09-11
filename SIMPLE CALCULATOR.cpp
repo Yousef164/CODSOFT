9f0954bdb9d761fbf30e0faa8d374db640c906fe
Yousef Salah <161240711+Yousef164@users.noreply.github.com>
2024-09-11 06:47:06 +0300

    SIMPLE CALCULATOR.cpp

---
+++ SIMPLE CALCULATOR.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cmath>
+#define ll long long
+using namespace std;
+class calculation{
+private:
+    ll a,b;
+    char c;
+public:
+    calculation(ll a, char c,ll b):a(a),b(b),c(c){}
+    void addition(){
+        cout<<a+b<<endl;
+    }
+    void subtraction(){
+        cout<<a-b<<endl;
+    }
+    void multiplication(){
+        cout<<a*b<<endl;
+    }
+    void division(){
+        cout<<(double)a/b<<endl;
+    }
+};
+int main(){
+    ll x,y;
+    char z;
+    cout<<"Enter the calculatio: ";
+    cin>>x>>z>>y;
+    calculation cal(x,z,y);
+    if(z=='+')
+        cal.addition();
+    else if(z=='-')
+        cal.subtraction();
+    else if(z=='*')
+        cal.multiplication();
+    else if(z=='/')
+        cal.division();
+    return 0;
+}
